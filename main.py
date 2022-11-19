272fc82c2de25c7d41cf226a115e050c364e8025
Hope | Revan <hugo.chassaing@orange.fr>
2022-11-19 22:42:30 +0000

    Changes to be committed: 	new file:   .gitignore 	modified:   main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import discord as discord
 from discord.ext import commands
-from discord.ext import tasks
 from datetime import datetime
 import asyncio
 import db_sqlite as to
@@ -21,7 +20,11 @@ async def on_ready():
 
 #Fonction qui log les print dans un fichier
 def log(text):
-    with open("ctf_log.txt", "a") as f:
+    with open("ctf_log.txt", "a", encoding="UTF-8") as f:
+        f.write("[" + str(datetime.now()) + "] > " + text + "\n")
+
+def a_log(text):
+    with open("admin_ctf_log.txt", "a", encoding="UTF-8") as f:
         f.write("[" + str(datetime.now()) + "] > " + text + "\n")
 
 #Create a ctf
@@ -33,11 +36,15 @@ async def create(ctx, name):
     to.create_thistory(name + ".db")
     await ctx.send("CTF created")
 
+    a_log("CTF created by " + str(ctx.author) + " : " + name)
+
 #Set active ctf
 @bot.command(name="setactive", description="Set active CTF")
 async def setactive(ctx, name):
     to.set_active_ctf(name)
     await ctx.send("CTF set active")
+
+    a_log("CTF set active by " + str(ctx.author) + " : " + name)
 
 #Add challenge to active ctf
 @bot.command(name="addchallenge", description="Add a challenge to the database")
@@ -53,6 +60,8 @@ async def add_challenge(ctx, nameid: str, points: int, flag: str):
 
     await ctx.send("Challenge ajouté !")
 
+    a_log("Challenge added by " + str(ctx.author) + " : " + nameid + " " + str(points) + " " + flag)
+
 #Show all challenges
 @bot.command(name="showchallenges", description="Show all challenges")
 async def show_challenges(ctx):
@@ -62,6 +71,8 @@ async def show_challenges(ctx):
     for challenge in challenges:
         msg += "NameID: " + challenge[1] + " | Name: " + challenge[2] + " | Points: " + str(challenge[3]) + " | Flag: " + str(challenge[5]) + " | History ASS: " + str(challenge[6]) + " |-| "
     await ctx.send(msg)
+
+    a_log("Challenges shown by " + str(ctx.author))
 
 #Show all history
 @bot.command(name="showhistory", description="Show all history")
@@ -73,6 +84,8 @@ async def show_history(ctx):
         msg += "ID : " + h[0] + "Content : " + h[1] + " || "
     
     await ctx.send(msg)
+
+    a_log("History shown by " + str(ctx.author))
 
 #Add name to challenge
 @bot.command(name="addname", description="Add a name to a challenge")
@@ -88,6 +101,8 @@ async def add_name(ctx, nameid: str, *, arg):
 
     await ctx.send("Nom ajouté !")
 
+    a_log("Name added by " + str(ctx.author) + " : " + nameid + " " + arg)
+
 #Add description to challenge
 @bot.command(name="adddescription", description="Add a description to a challenge")
 async def add_description(ctx, nameid: str, *, arg):
@@ -98,9 +113,15 @@ async def add_description(ctx, nameid: str, *, arg):
     #    return
 
     ctf = to.get_active_ctf()[0]
-    to.update_description(ctf + ".db", nameid, arg)
+    try:
+        to.update_description(ctf + ".db", nameid, arg)
+    except:
+        await ctx.send("Le nom du challenge est incorrect ou le CTF n'est pas actif")
+        return
 
     await ctx.send("Description ajoutée !")
+
+    a_log("Description added by " + str(ctx.author) + " : " + nameid + " " + arg)
 
 #Add points to challenge
 @bot.command(name="addpoints", description="Add points to a challenge")
@@ -112,9 +133,15 @@ async def add_points(ctx, nameid: str, points: int):
     #    return
 
     ctf = to.get_active_ctf()[0]
-    to.update_points(ctf + ".db", nameid, points)
+    try:
+        to.update_points(ctf + ".db", nameid, points)
+    except:
+        await ctx.send("Le nom du challenge est incorrect ou le CTF n'est pas actif")
+        return
 
     await ctx.send("Points ajoutés !")
+
+    a_log("Points added by " + str(ctx.author) + " : " + nameid + " " + str(points))
 
 #Add flag to challenge
 @bot.command(name="addflag", description="Add a flag to a challenge")
@@ -126,9 +153,15 @@ async def add_flag(ctx, nameid: str, flag: str):
     #    return
 
     ctf = to.get_active_ctf()[0]
-    to.update_flag(ctf + ".db", nameid, flag)
+    try:
+        to.update_flag(ctf + ".db", nameid, flag)
+    except:
+        await ctx.send("Le nom du challenge est incorrect ou le CTF n'est pas actif")
+        return
 
     await ctx.send("Flag ajouté !")
+
+    a_log("Flag added by " + str(ctx.author) + " : " + nameid + " " + flag)
 
 #Add history to challenge
 @bot.command(name="addhistory", description="Add a history to a challenge")
@@ -140,9 +173,15 @@ async def add_history(ctx, nameid: str, dh: str):
     #    return
 
     ctf = to.get_active_ctf()[0]
-    to.update_history(ctf + ".db", nameid, dh)
+    try:
+        to.update_history(ctf + ".db", nameid, dh)
+    except:
+        await ctx.send("Le nom du challenge est incorrect ou le CTF n'est pas actif")
+        return
 
-    await ctx.send("Historique ajouté !")
+    await ctx.send("Histoire lié au challenge !")
+
+    a_log("History linked by " + str(ctx.author) + " : " + nameid + " " + dh)
 
 #Add history to history
 @bot.command(name="addhth", description="Add a history to a history")
@@ -154,9 +193,15 @@ async def add_history_to_history(ctx, *, arg):
     #    return
 
     ctf = to.get_active_ctf()[0]
-    to.add_history(ctf + ".db", arg)
+    try:
+        to.add_history(ctf + ".db", arg)
+    except:
+        await ctx.send("Attention ! Vous avez peut-être oublié de mettre le CTF en actif !")
+        return
 
     await ctx.send("Histoire ajouté !")
+
+    a_log("History added by " + str(ctx.author) + " : " + nameid + " " + dh)
 
 #Start CTF and send message to all users
 @bot.command(name="startctf", description="Start CTF")
@@ -176,6 +221,10 @@ async def startctf(ctx):
 
     await ctx.send("CTF started !")
 
+    a_log("CTF started by " + str(ctx.author))
+
+    
+
 ######################################
 ## Play commands #####################
 ######################################
@@ -191,11 +240,17 @@ async def add_user(ctx):
     autid = ctx.author.id
 
     ctf = to.get_active_ctf()[0]
-    to.add_user(ctf + ".db", autid)
+    try:
+        to.add_user(ctf + ".db", autid)
+    except:
+        await ctx.send("Tu es déjà inscrit au CTF ou il y a un soucis !")
+        return
 
     embed = discord.Embed(title="CTF - OKA", description="Bienvenue !", color=0x00ff00)
     embed.add_field(name="Bienvenue dans ce CTF !", value="Vous avez rejoint avec votre compte Discord : " + str(ctx.author), inline=False)
     await ctx.send(embed=embed)
+
+    a_log("User " + str(ctx.author) + " joined the CTF.")
 
 #Check flag
 @bot.command(name="submit", description="Check a flag")
@@ -260,6 +315,8 @@ async def show_classement(ctx):
         embed.add_field(name=str(pt) + " - " + str(bot.get_user(int(i[0]))), value="Points : " + str(i[1]), inline=False)
     await ctx.send(embed=embed)
 
+    a_log("Classement shown by " + str(ctx.author))
+
 @bot.command(name="credits", description="Show the credits")
 async def show_credits(ctx):
     if ctx.author.bot:
@@ -269,6 +326,8 @@ async def show_credits(ctx):
     embed.add_field(name="Créateurs du CTF", value="Saber | XBORZ#2588 -- Thomas | ThomasM#4405", inline=False)
     embed.add_field(name="Créateur de OKA - Bot Discord", value="Rhenar | Hope#5432", inline=False)
     await ctx.send(embed=embed)
+
+    a_log("Credits shown by " + str(ctx.author))
 
 @bot.command(name="endctf", description="End CTF")
 async def endctf(ctx):
@@ -294,6 +353,8 @@ async def endctf(ctx):
 
     await ctx.send("CTF ended !")
 
+    a_log("CTF ended by " + str(ctx.author))
+
 @bot.command("initall", description="Init all")
 async def initall(ctx):
     if ctx.author.bot:
@@ -305,7 +366,9 @@ async def initall(ctx):
     to.create_ctf()
     await ctx.send("Init all done")
 
-with open('config.json') as config_file:
+    a_log("Init all done by " + str(ctx.author))
+
+with open('config.json', encoding="UTF-8") as config_file:
     config = json.load(config_file)
 
 bot.run(config[0]['token'])
